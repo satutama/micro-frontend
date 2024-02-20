dceb024e3d9d14ca2ef07e85cf3c0c1e40260263
Satria Utama <satria.u.s@gmail.com>
2024-02-20 18:48:25 +0100

    feat: 🎸 dashboard component
    with angular material and material icons added

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,14 +1,32 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgClass,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+  ],
   selector: 'ng-mf-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dashboard';
+  public routes = [...appRoutes].filter((route) => !!route.title);
 }
--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'qr-scanner',
+    title: 'QR scanner',
     loadChildren: () => import('qr-scanner/Routes').then((m) => m.remoteRoutes),
   },
   {
