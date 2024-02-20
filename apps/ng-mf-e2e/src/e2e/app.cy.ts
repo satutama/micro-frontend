5a9e6d644f62490398a4643dfece6905aaad5ac4
Satria Utama <satria.u.s@gmail.com>
2024-02-20 16:03:11 +0100

    integrated monorepo

--- apps/ng-mf-e2e/src/e2e/app.cy.ts
+++ apps/ng-mf-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('e2e', () => {
+describe('ng-mf-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
