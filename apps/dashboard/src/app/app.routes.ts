f187316cd35487af8af22f2d3c222870b65326dd
Satria Utama <satria.u.s@gmail.com>
2024-02-27 12:39:31 +0100

    adjust route

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
   {
-    path: 'accordion',
-    title: 'Accordion',
-    loadChildren: () => import('accordion/Routes').then((m) => m.appRoutes),
-  },
-  {
     path: 'qr-code',
     title: 'QR code',
     loadChildren: () => import('qr-scanner/Routes').then((m) => m.appRoutes),
+  },
+  {
+    path: 'accordion',
+    title: 'Accordion',
+    loadChildren: () => import('accordion/Routes').then((m) => m.appRoutes),
   },
   {
     path: '',
