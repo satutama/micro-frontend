f4973d356ee56a71a72d423d4ff06f74749a368b
Satria Utama <satria.u.s@gmail.com>
2024-03-01 16:02:43 +0100

    remove entry component

--- apps/age-calculator/module-federation.config.ts
+++ apps/age-calculator/module-federation.config.ts
@@ -3,7 +3,7 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'age-calculator',
   exposes: {
-    './Routes': 'apps/age-calculator/src/app/remote-entry/entry.routes.ts',
+    './Routes': 'apps/age-calculator/src/app/app.routes.ts',
   },
 };
 
--- apps/age-calculator/src/app/app.routes.ts
+++ apps/age-calculator/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Route } from '@angular/router';
+import { AgeCalculatorComponent } from './app.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    loadChildren: () =>
-      import('./remote-entry/entry.routes').then((m) => m.remoteRoutes),
+    component: AgeCalculatorComponent,
   },
 ];
